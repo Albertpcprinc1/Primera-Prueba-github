1202d2ad7ee685369390511bbe491a9dd4d4b1f1
Albertpcprinc1 <achm.comp@gmail.com>
2026-02-10 23:14:32 -0500

    agregue nueva clase en entity 2

---
+++ src/main/java/com/hibernate/ferreteria/entity/prueba2_git_a_github.java
@@ -0,0 +1,12 @@
+package com.hibernate.ferreteria.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Table(name="articulos")
+@Data
+public class prueba2_git_a_github {
+
+}
